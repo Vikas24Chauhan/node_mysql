2efea6a146e1cb1e31b58987f61c7a2ae32cfe14
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-07-23 10:57:19 +0530

    Environment Variables (.env)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+import "./config/env.js";
 import app from "./app.js";
-import "./config/db.js";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
