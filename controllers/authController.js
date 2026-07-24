9300e742e5d36f55e31036e643528299766a447b
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-07-24 10:15:19 +0530

    login API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import pool from "../config/db.js";
 import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -31,6 +32,49 @@ export const register = async (req, res, next) => {
       success: true,
       message: "User registered successfully",
       userId: result.insertId,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (rows.length === 0) {
+      const error = new Error("Invalid email or password");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    const user = rows[0];
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      const error = new Error("Invalid email or password");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    // Generate Token
+    const token = generateToken(user);
+
+    // Remove password before sending user data
+    delete user.password;
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user,
     });
   } catch (error) {
     next(error);
