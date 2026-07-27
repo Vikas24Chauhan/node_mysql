4f76b775e08e7b11503265b374ae6582dcfaaf27
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-07-27 11:49:03 +0530

    Login frontend integration done

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorHandler from "./middlewares/errorMiddleware.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+// Enable CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 // Routes
 app.use("/users", userRoutes);
