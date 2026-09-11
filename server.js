b49582e0ef22e0ecf133937ac59f4e1aa7417efd
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-09-11 12:01:29 +0530

    try 3

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
-
 import "./config/env.js";
 import app from "./app.js";
 
--- services/emailService.js
+++ services/emailService.js
@@ -1,38 +1,7 @@
-// import nodemailer from "nodemailer";
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// export const sendEmail = async (to, subject, html) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: process.env.EMAIL_USER,
-//       to,
-//       subject,
-//       html,
-//     });
-
-//     console.log("Email sent:", info.messageId);
-
-//     return info;
-//   } catch (error) {
-//     console.error("Email sending failed:", error);
-//     throw error;
-//   }
-// };
-
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.hostinger.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
