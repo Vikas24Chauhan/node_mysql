001a1a4d1845b0db377b244add1f433028349a63
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-09-11 10:31:22 +0530

    email service code updated

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,6 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -10,10 +9,19 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.messageId);
+
+    return info;
+  } catch (error) {
+    console.error("Email sending failed:", error);
+    throw error;
+  }
 };
